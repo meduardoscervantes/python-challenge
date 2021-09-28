d2be84e62b7d717919bb08c84709b46827a467fa
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-09-27 22:37:15 -0500

    Stopping point for the PyBank project. Need to finish the last half of the assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,17 +11,22 @@ import csv
 #find the greatest decrease in profits and print : {when, amount}
 
 
-
+print("Financial Analysis")
+print("----------------------------")
 path = r'H:\2021 UTSA Bootcamp\HW_3_MESC\PyBank\Resources\budget_data.csv'
 with open(path) as data:
         rawData = data.read()
         rows = rawData.split("\n")
+        mTotal = 0
         amount = []
         dates = []
-        for i in range(2,len(rows)):
+        #seperate the data into its respective categories
+        for i in range(1, len(rows)-1):
             temp = rows[i]
             dates.append(temp[:8])
             amount.append(temp[9:])
-        
-        print(dates)
-        print(amount)
+        print(f"Total Months: {len(dates)}")
+        #find the total
+        for x in amount:
+            mTotal += int(x)
+        print(f"Total: ${mTotal}")
