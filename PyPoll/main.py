39a4dfd0a651416b4121e86cb327a8fd258d5a80
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-10-02 11:13:01 -0500

    Updated the PyPoll to use the dictionary class. Updated how the code tallies the votes and keeps track of information inside of Python.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,11 +9,9 @@ with open(path) as data:
     voterId = []
     candidate = []
     county = []
-    khanVotes = 0
-    correyVotes = 0
-    liVotes = 0
-    tooleyVotes = 0
-    winner = 0
+    tallyVotes = dict()
+    wName = "NA"
+    wVal = 0
     rows = rawData.split("\n")
     for i in range(1, len(rows) - 1):
         temp = rows[i].split(',')
@@ -24,35 +22,29 @@ with open(path) as data:
                 county.append(temp[j])
             if j == 2:
                 candidate.append(temp[j])
+    f = open("PyPoll_Output.txt", "a")
     print(f"Total Votes: {len(voterId)}")
     print("-------------------------")
-    for i in range(len(candidate)):
-        if candidate[i] == "Khan":
-            khanVotes += 1
-        elif candidate[i] == "Correy":
-            correyVotes += 1
-        elif candidate[i] == "Li":
-            liVotes += 1
-        elif candidate[i] == "O'Tooley":
-            tooleyVotes += 1
-    print(f"Khan: {round(khanVotes/len(candidate)*100,4)}% ({khanVotes})")
-    print(f"Correy: {round(correyVotes/len(candidate)*100,4)}% ({correyVotes})")
-    print(f"Li: {round(liVotes/len(candidate)*100,4)}% ({liVotes})")
-    print(f"O'Tooley: {round(tooleyVotes/len(candidate)*100,4)}% ({tooleyVotes})")
-    print("-------------------------")
-    print("Winner: Khan")
-    print("-------------------------")
-    f = open("PyPoll_Output.txt", "a")
     f.write("Election Results")
     f.write("\n-------------------------")
     f.write(f"\nTotal Votes: {len(voterId)}")
     f.write("\n-------------------------")
-    f.write(f"\nKhan: {round(khanVotes/len(candidate)*100,4)}% ({khanVotes})")
-    f.write(f"\nCorrey: {round(correyVotes/len(candidate)*100,4)}% ({correyVotes})")
-    f.write(f"\nLi: {round(liVotes/len(candidate)*100,4)}% ({liVotes})")
-    f.write(f"\nO'Tooley: {round(tooleyVotes/len(candidate)*100,4)}% ({tooleyVotes})")
+    for i in candidate:
+        if i in tallyVotes:
+            tallyVotes[i] += 1
+        else:
+            tallyVotes[i] = 1
+    for i in tallyVotes:
+        print(f"{i}: {round((tallyVotes.get(i) / len(candidate)) * 100, 4) }% ({tallyVotes.get(i)})")
+        f.write(f"\n{i}: {round((tallyVotes.get(i) / len(candidate)) * 100, 4) }% ({tallyVotes.get(i)})")
+        if tallyVotes[i] > wVal:
+            wName = i
+            wVal = tallyVotes[i]
+    print("-------------------------")
+    print(f"Winner: {wName}")
+    print("-------------------------")
     f.write("\n-------------------------")
-    f.write("\nWinner: Khan")
+    f.write(f"\nWinner: {wName}")
     f.write("\n-------------------------")
     f.close()
     data.close()
